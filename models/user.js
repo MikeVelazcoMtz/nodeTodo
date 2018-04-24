11fbbc46a5fac170138c9b26a1c535f589a8db05
Miguel Angel Velazco martinez <velazcomtz.miguel@gmail.com>
2018-04-24 10:36:45 -0500

    user model modification

--- models/user.js
+++ models/user.js
@@ -1,18 +1,41 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
 
-var userSchema = new Schema({
-    _id: Schema.Types.ObjectId,
-    userName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    items: [{type: Schema.Types.ObjectId, ref: 'ListItem'}]
-});
+var UserSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  items: [{type: Schema.Types.ObjectId, ref: 'ListItem'}]
+})
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.pre('save', function (next) {
+  var user = this
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next()
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err)
+
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err)
+
+      // override the cleartext password with the hashed one
+      user.password = hash
+      next()
+    })
+  })
+})
+
+module.exports = mongoose.model('User', UserSchema)
