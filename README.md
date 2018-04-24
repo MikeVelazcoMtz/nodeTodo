60686b33fc11549bd7f7574d0a0dfb6a36b00a1a
Miguel Angel Velazco martinez <velazcomtz.miguel@gmail.com>
2018-04-24 10:33:51 -0500

    Docker integration

---
+++ README.md
@@ -0,0 +1,9 @@
+# nodeTodo
+
+This is a very basic express application that implements a TO-DO list REST API that uses JSON web tokens for authentication.
+
+## Run as a development environment
+
+To run this on a development environment you must install docker
+
+Having this you only need to run `docker-compose up -d` and everything is set.
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,41 @@ var jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator/check')
 const validatorUtil = require('express-validator').utils
 const {matchedData, sanitize} = require('express-validator/filter')
+const User = require('../models/user')
 
 var router = express.Router()
 
 module.exports = function (secret) {
-  router.get('/home', function (req, res) {
-    res.send('<h1>Hello!</h1><p>Welcome to Express.js</p>')
+  router.get('/sign_in', function (req, res) {
+    // Validate user name
+    req.checkBody('username')
+      .exists().withMessage('Username is required')
+      .notEmpty().withMessage('Username can\'t be empty')
+      .isAlphanumeric('es-ES').withMessage('Username format is not valid')
+      .isLength({min: 3, max: 50}).withMessage('Username must be between 3 and 50 characters long')
+
+    req.checkBody('password')
+      .isAlphanumeric('es-ES').withMessage('Username format is not valid')
+      .isLength({min: 3, max: 50}).withMessage('password must be between 3 and 50 characters long')
+    req.checkBody('password_confirmation')
+      .isAlphanumeric('es-ES').withMessage('passowrd format is not valid')
+      .isLength({min: 3, max: 50}).withMessage('password must be between 3 and 50 characters long')
+
+    var errors = req.validationErrors()
+
+    if (errors) {
+      res.status(400)
+      res.send(errors)
+    } else {
+      username = req.sanitize('username').trim()
+      password = req.sanitize('password')
+      confirm = req.sanitize('password_confirmation')
+
+      if (password != confirm) {
+        res.status(400)
+        res.send({message: 'password and password_confirmation must be equals'})
+      }
+    }
   })
 
   router.post('/login', function (req, res) {
@@ -28,6 +57,7 @@ module.exports = function (secret) {
     var errors = req.validationErrors()
 
     if (errors) {
+      res.status(400)
       res.send(errors)
     } else {
       username = req.sanitize('username').trim()
